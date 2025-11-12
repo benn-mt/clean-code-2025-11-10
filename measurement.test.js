a325b48b320236b95c2e1c88bff2b9c476326a12
Ben Noble <ben.noble@madetech.com>
2025-11-12 11:52:36 +0000

    most measurements done

--- measurement.test.js
+++ measurement.test.js
@@ -1,5 +1,5 @@
 const {Measurement} = require("./measurement")
-const {TEASPOON, TABLESPOON} = require("./volumes")
+const {TEASPOON, TABLESPOON, OUNCE, CUP, PINT} = require("./volumes")
 
 describe('Measurement...', () => {
     test('Can be compared for equality...', () => {
@@ -10,5 +10,16 @@ describe('Measurement...', () => {
         expect(new Measurement(2, TABLESPOON).equals(new Measurement(3, TEASPOON))).toBe(false)
         expect(new Measurement(2, TABLESPOON).equals(new Measurement(6, TEASPOON))).toBe(true)
         expect(new Measurement(3, TEASPOON).equals(new Measurement(1, TABLESPOON))).toBe(true)
+
+        expect(new Measurement(1, OUNCE).equals(new Measurement(1, OUNCE))).toBe(true)
+        expect(new Measurement(1, OUNCE).equals(new Measurement(6, TEASPOON))).toBe(true)
+        expect(new Measurement(6, TEASPOON).equals(new Measurement(1, OUNCE))).toBe(true)
+    })
+
+    test('Can be compared for equality...', () => {
+        expect(new Measurement(1, TABLESPOON).equals(new Measurement(3, TEASPOON))).toBe(true)
+        expect(new Measurement(1, OUNCE).equals(new Measurement(2, TABLESPOON))).toBe(true)
+        expect(new Measurement(1, CUP).equals(new Measurement(8, OUNCE))).toBe(true)
+        expect(new Measurement(1, PINT).equals(new Measurement(2, CUP))).toBe(true)
     })
 })
--- volumes.js
+++ volumes.js
@@ -1,6 +1,11 @@
 class Unit{
-    constructor(ratioToTeaspoons){
-        this._ratioToTeaspoons = ratioToTeaspoons
+    constructor(ratio, ratioUnit){
+        if (ratioUnit){
+            this._ratioToTeaspoons = ratio * ratioUnit._ratioToTeaspoons
+        }
+        else {
+            this._ratioToTeaspoons = 1
+        }
     }
 
     quantityInTeaspoons(quantity){
@@ -8,7 +13,13 @@ class Unit{
     }
 }
 
-const TEASPOON = new Unit(1)
-const TABLESPOON = new Unit(3)
 
-module.exports = {TEASPOON, TABLESPOON}
+const TEASPOON = new Unit()
+const TABLESPOON = new Unit(3, TEASPOON)
+const OUNCE = new Unit(2, TABLESPOON)
+const CUP = new Unit(8, OUNCE)
+const PINT = new Unit(2, CUP)
+const QUART = new Unit(2, PINT)
+const MILLILITRE = new Unit(0.203, TEASPOON)
+
+module.exports = {TEASPOON, TABLESPOON, OUNCE, CUP, PINT}
